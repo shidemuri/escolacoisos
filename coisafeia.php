f5dc49f684522abac14f24a8e0ea606d0df7f35f
padero <68622956+shidemuri@users.noreply.github.com>
2024-03-06 12:42:51 -0300

    Update coisafeia.php

--- coisafeia.php
+++ coisafeia.php
@@ -1,8 +1,8 @@
 <?php
     $rawdir = 'C:\\';
-    $dir = scandir('C:\\');
+    $dir = scandir($rawdir);
     if(isset($_REQUEST['path'])){
-        $rawdir = realpath($_GET['path']) ? realpath($_GET['path']) : $_GET['path'];
+        $rawdir = str_replace("\ ","%20",realpath($_GET['path']) ? realpath($_GET['path']) : $_GET['path']);
         if(is_dir($_GET['path'])) {
             $dir = scandir($rawdir);
         } else {
@@ -26,10 +26,22 @@
             die();
         };
     };
+
+    function human_filesize($bytes, $decimals = 2) {
+        $factor = floor((strlen($bytes) - 1) / 3);
+        if ($factor > 0) $sz = 'KMGT';
+        return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor - 1] . 'B';
+    }
+
     function boiola($har){
         header('Location: ' . $_SERVER['PHP_SELF'] . '?path=' . $har, true, 303);
         exit();
     }
+
+    $voltado = preg_split("/[\\\\]+/", $rawdir);
+    unset($voltado[count($voltado)-1]);
+    $voltado = join('\\',$voltado);
+
     if(isset($_GET['action'])) {
         switch($_GET['action']){
             case 'rename':
@@ -43,8 +55,7 @@
             #break;
             case 'delete': 
                 unlink($rawdir);
-                $e[count($e)-1] = '';
-                boiola(join('\\',$e));
+                boiola($voltado);
             break;
         };
     };
@@ -62,11 +73,24 @@
                     echo 'false';
                 }
             break;
+            case 'edit':
+                if(!isset($_POST['content'])){
+                    echo "<h1> calhorda </h1>";
+                } elseif(!is_readable($rawdir)) {
+                    echo "<h1> Não é possível escrever para este arquivo (é pasta / não é editável) </h1>";
+                } else {
+                    file_put_contents($rawdir, $_POST['content']);
+                    echo "<h1> editado com sucesso </h1>";
+                };
+            break;
         };
     };
     
+
+    
     #echo var_dump($_SERVER);
-    echo $rawdir;
+    echo '<h3>' . $rawdir . '</h3>';
+    echo '<style> *{font-size:18px} </style>';
     if(is_dir($rawdir)) {?>
         <form action="" method="post" enctype="multipart/form-data">
             <input type="hidden" name="action" value="upload">
@@ -84,10 +108,27 @@
             ?><?=$dir[$i]?></a></p>
     <?php
         };
-    } else {
+    } elseif(isset($_REQUEST['action']) && $_REQUEST['action'] == 'edit'){ ?>
+
+        <p><a href="?path=<?=$rawdir?>"> Voltar </a></p>
+        <?=is_writable($rawdir) ? '' : "<h1> Não é possível escrever para este arquivo (é pasta / não é editável) </h1>"?>
+        <div style="display:flex;flex-direction:column;">
+            <form action="" method="post">
+                <input type="hidden" name="path" value="<?=$rawdir?>">
+                <input type="hidden" name="action" value="edit">
+                <textarea spellcheck="false" name="content" cols="60" rows="30" <?=is_writable($rawdir) ? '' : 'readonly'?>><?=file_get_contents($rawdir)?></textarea>
+                <p>
+                    <input style="font-size:20px;" type="submit" value="editar">
+                </p>
+            </form>
+        </div>
+    <?php } else {
         ?>
         <div>
-            <a href="?path=<?=$rawdir?>&action=delete"> Deletar </a>
+            <h2><?=human_filesize(filesize($rawdir))?></h2>
+            <p><a href="?path=<?=$voltado?>"> Voltar </a></p>
+            <p><a href="?path=<?=$rawdir?>&action=delete"> Deletar </a></p>
+            <p><a href="?path=<?=$rawdir?>&action=edit"> Editar como texto </a></p>
             <button id="rename">Renomear</button>
             <form action="" id="rform">
                 <input type="hidden" name="path" value="<?=$rawdir?>">
@@ -103,6 +144,9 @@
                 <input type="hidden" name="dl" value="1">
                 <input type="submit" value="baixar">
             </form>
+            
+            <?=!is_writable($rawdir) ? '<h2 style="background-color:red;"> kono fairu wa readonly desu (nihongo jouzu) </h2>' : ''?>
+
             <script>
                 document.querySelector('#rename').onclick = function(){
                     const porro = prompt("Digite um novo nome para <?=$e[count($e)-1]?>")
